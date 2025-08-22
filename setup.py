025df40a215aef4bea80cbc2614d94d0a28bf43e
Luibbo <lubomyrbotckiv@gmail.com>
2025-08-22 17:19:01 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='MedicalChatbot',
+    version='0.0.0',
+    author='Liubomyr',
+    author_email='lubomyrbotckiv@gmail.com',
+    packeges=find_packages(),
+    install_requires=[] 
+)
